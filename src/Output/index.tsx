fdbb213eb2a400eb8d72cc99b52a13afbd466777
KazukiAmatatsu <80532711+KazukiAmatatsu@users.noreply.github.com>
2021-10-09 23:51:13 +0900

    .

--- src/Output/index.tsx
+++ src/Output/index.tsx
@@ -1,33 +1,37 @@
 import { FC } from 'react'
 import { Heading } from 'src/components/Heading'
+import OutputCard from './OutputCard'
 import IndianPoker from './IndianPoker.mdx'
 import Tagiron from './Tagiron.mdx'
 import styled from 'styled-components'
 import { sp } from 'styles/media'
 
 import dynamic from 'next/dynamic'
-const OutputCard = dynamic(() => import('src/Output/OutputCard'), { ssr: false })
+const ScrollRevealContainer = dynamic(import('src/components/ScrollRevealContainer'), { ssr: false })
 
 const Output: FC = () => {
   return (
     <StyledOutput>
       <Heading id='output'>Output</Heading>
-      <OutputCard
-        className='left'
-        src={'/indianPoker.gif'}
-        url={'https://indian-poker-six.vercel.app/'}
-        github_url={'https://github.com/KazukiAmatatsu/indian-poker'}
-      >
-        <IndianPoker />
-      </OutputCard>
-      <OutputCard
-        className='right'
-        // src={''}
-        // url={''}
-        // github_url={''}
-      >
-        <Tagiron />
-      </OutputCard>
+      <ScrollRevealContainer move='left'>
+        <OutputCard
+          src={'/indianPoker.gif'}
+          url={'https://indian-poker-six.vercel.app/'}
+          github_url={'https://github.com/KazukiAmatatsu/indian-poker'}
+        >
+          <IndianPoker />
+        </OutputCard>
+      </ScrollRevealContainer>
+      <ScrollRevealContainer move='right'>
+        <OutputCard
+          className='right'
+          // src={''}
+          // url={''}
+          // github_url={''}
+        >
+          <Tagiron />
+        </OutputCard>
+      </ScrollRevealContainer>
     </StyledOutput>
   )
 }
@@ -40,8 +44,10 @@ const StyledOutput = styled.div`
   h2 {
     color: ${(props) => props.theme.colors.white};
   }
-  .right {
+  section:nth-child(n + 2) {
     margin-top: 5rem;
+  }
+  .right {
     margin-left: auto;
   }
   ${sp`
